8a542639104f9e1d454088ed4d9a55a799646819
compumike08 <compumike08@users.noreply.github.com>
2015-10-22 14:49:42 -0400

    Add code to use billAmountsOwedByBill service and display on bill-detail.html view

--- src/main/webapp/scripts/app/entities/bill/bill-detail.controller.js
+++ src/main/webapp/scripts/app/entities/bill/bill-detail.controller.js
@@ -1,13 +1,20 @@
 'use strict';
 
 angular.module('billTrackerApp2App')
-    .controller('BillDetailController', function ($scope, $rootScope, $stateParams, entity, Bill, BillAmountsOwed, BillTransactionLedger) {
+    .controller('BillDetailController', function ($scope, $rootScope, $stateParams, entity, Bill, BillAmountsOwed, BillAmountsOwedByBill, BillTransactionLedger) {
         $scope.bill = entity;
         $scope.load = function (id) {
             Bill.get({id: id}, function(result) {
                 $scope.bill = result;
             });
+
+            BillAmountsOwedByBill.query({id: id}, function(result){
+                $scope.bill.billAmountsOwed = result;
+            });
         };
+
+        $scope.load($scope.bill.id);
+
         $rootScope.$on('billTrackerApp2App:billUpdate', function(event, result) {
             $scope.bill = result;
         });
